23c4a154a5d94584929b33a7339d108dad9e7b2a
Joan Casa Nueva PC <joanroycar@gmail.com>
2024-10-15 21:46:30 -0500

    UDAPTE

--- storage/framework/views/cba751e098fe4f229a536d38f1bfa7d3.php
+++ storage/framework/views/cba751e098fe4f229a536d38f1bfa7d3.php
@@ -227,12 +227,7 @@
     <div class="drag-target"></div>
 
     <!-- BEGIN: Footer-->
-    <footer class="footer footer-static footer-light">
-        <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy;
-                2023<a class="ms-25" href="https://web.munisjl.gob.pe/web/" target="_blank">MUNI SJL</a><span
-                    class="d-none d-sm-inline-block">, Todo los derechos reservados.</span></span><span
-                class="float-md-end d-none d-md-block">jcarlos@munisjl.gob.pe<i data-feather="heart"></i></span></p>
-    </footer>
+   
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
     <!-- END: Footer-->
     <script src="/app-assets/vendors/js/vendors.min.js"></script>
